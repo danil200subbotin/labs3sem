6b3154b8d2e42eba64396ef42ec312b734c14cf9
danil200subbotin <danil200subbotin@gmail.com>
2020-10-14 01:07:15 +0300

    настройка этого гита

---
+++ lab3byte2/biglong.cpp
@@ -0,0 +1,321 @@
+//
+// Created by Данил Морозов on 06/10/2020.
+//
+
+#include "biglong.h"
+#include <cmath>
+#include <iostream>
+//#include <cstring>
+
+namespace laba3bit1 {
+
+    Biglong::Biglong(const Biglong &biglong) {
+        Biglong::length = biglong.get_length();
+        for (int i = 0; i <= MAX_LENGTH; ++i) {
+            Biglong::value[i] = char(biglong.get_one_char(i));
+        }
+    }
+
+
+    Biglong::Biglong() : length(0) {
+        for (int i = 0; i <= MAX_LENGTH; ++i) {
+            value[i] = '0';
+        }
+    };
+
+    Biglong::Biglong(long parametr) {
+        //       std::cout << "я тут" << std::endl;
+        int counter = 0;
+        long parametr_copy = parametr;
+        if (parametr > 0) {
+            Biglong::value[0] = '0';
+        } else {
+            Biglong::value[0] = '9';
+            parametr_copy = -parametr_copy;
+        }
+        while (parametr_copy > 0) {
+            //     std::cout << "изменяю: " << MAX_LENGTH - counter << " НА " << parametr_copy % 10 << std::endl;
+            if (MAX_LENGTH - counter < 1)
+                throw "подано слишком большое значение long";
+            Biglong::value[MAX_LENGTH - counter] = num_to_char(parametr_copy % 10);
+            parametr_copy = parametr_copy / 10;
+            ++counter;
+        }
+        Biglong::length = counter;
+        for (int i = 1; i <= MAX_LENGTH - counter; ++i) {
+            Biglong::value[i] = '0';
+        }
+    }
+
+
+    Biglong::Biglong(const char *char_value) {
+        int i = int(strlen(char_value)), j = 0;
+        //    std::cout << i << std::endl;
+
+
+        if (char_value[0] == '-') {
+            value[0] = '9';
+            j++;
+            --i;
+        } else {
+            value[0] = '0';
+            if (((int) char_value[0] < '0') || ((int) char_value[0] > '9')) {
+                throw "Попытка инициализировать класс некорректной строкой";
+            }
+        }
+        if (i > MAX_LENGTH)
+            throw "попытка присвоить объекту слишком длинной строки";
+        Biglong::length = i;
+        while (char_value[j] != '\0') {
+            if (((int) char_value[j] >= '0') & ((int) char_value[j] <= '9')) {
+                value[MAX_LENGTH - i + 1] = char_value[j];
+                --i;
+                ++j;
+
+            } else {
+                std::cout << "В строке встретился не числовой символ" << std::endl;
+                throw "В строке встретился не числовой символ";
+            }
+        }
+        for (int i = 1; i <= MAX_LENGTH - Biglong::length; ++i) {
+            Biglong::value[i] = '0';
+        }
+
+    }
+
+    void laba3bit1::Biglong::div10() {
+        for (int i = MAX_LENGTH; i > 1; --i) {
+            Biglong::value[i] = Biglong::value[i - 1];
+        }
+    }
+
+    void laba3bit1::Biglong::multi10() {
+        if (Biglong::value[1] == '0') {
+            for (int i = 1; i < MAX_LENGTH; ++i) {
+                Biglong::value[i] = Biglong::value[i + 1];
+            }
+            Biglong::value[MAX_LENGTH] = '0';
+        } else
+            throw "невозможно выполнить умножение из-за опасности переполнения стека";
+    }
+
+
+
+
+//const laba3bit1::Biglong operator~ () const {
+
+//----------------------------------------------------------------------тут пошла часть с перегрузками-----------------
+
+
+//вывод класса в поток
+    std::ostream &operator<<(std::ostream &stream, const laba3bit1::Biglong &biglong) {
+        //std::cout << "я тут2";
+        int i = 1;
+        if (biglong.get_one_char(0) == '9')
+            stream << "-";
+        while (i <= biglong.get_max_value()) {
+            stream << biglong.get_one_char(i);
+            ++i;
+        }
+        stream << std::endl;
+        return stream;
+    }
+
+
+    std::istream &operator>>(std::istream &istream, laba3bit1::Biglong &biglong) {
+        for (int k = 1; k <= biglong.get_max_value(); ++k) {
+            biglong.set_one_char(k, 0);
+        }
+        std::cout << "tut" << std::endl;
+        char streamer[biglong.get_max_value() + 1];
+        istream
+                >> streamer;                                                //здесь может быть считывание нормальной строки
+        int i = int(strlen(streamer)), j = 0;
+        std::cout << "i = " << i << std::endl;
+        if (i > biglong.get_max_value())
+            throw "попытка присвоить объекту слишком длинной строки";
+        if (streamer[0] == '-') {
+            biglong.set_one_char(0, 1);
+            j++;
+            --i;
+        } else {
+            biglong.set_one_char(0, 0);
+            if (((int) streamer[0] < '0') || ((int) streamer[0] > '9')) {
+                throw "Попытка инициализировать класс некорректной строкой";
+            }
+        }
+        biglong.set_length(i);
+        while (streamer[j] != '\0') {
+            if (((int) streamer[j] >= '0') & ((int) streamer[j] <= '9')) {
+                std::cout << "заменяю: " << biglong.get_max_value() - i + 1 << " на: " << streamer[j] << std::endl;
+                biglong.set_one_char(biglong.get_max_value() - i + 1, char_to_num(streamer[j]));
+                --i;
+                ++j;
+            } else {
+                std::cout << "В строке встретился не числовой символ" << std::endl;
+                throw "В строке встретился не числовой символ";
+            }
+        }
+        return istream;
+    }
+
+
+    const Biglong Biglong::operator~() const {                      //здесь еще нужно написать прибавление единички
+        Biglong newest(*this);
+        if ((newest.get_one_char(0) == '9') || (newest.get_one_char(0) == '1')) {
+            newest.set_one_char(0, 9);
+            for (int i = 1; i <= MAX_LENGTH; ++i) {
+                newest.set_one_char(i, 9 - char_to_num(newest.get_one_char(i)));
+            }
+            ++newest;
+        }
+        return newest;
+    };
+
+
+    Biglong &Biglong::operator++() {
+        int a = 0, ostatok = 1, i = MAX_LENGTH;
+        while ((i > 0) & (ostatok > 0)) {
+            a = char_to_num(value[i]);
+            a = ostatok + a;
+            value[i] = num_to_char(a % 10);
+            ostatok = a / 10;
+            --i;
+        }
+        if ((i < 1) & (ostatok != 0)) {
+            throw "переполнение стека";
+        }
+        return *this;
+    }
+
+
+    const Biglong Biglong::operator+(const Biglong &argument) const {
+        Biglong result(*this);
+        result = ~result;
+        int a = 0, b = 0, over = 0;
+        Biglong argument_copy;
+        argument_copy = ~argument;
+        for (int i = MAX_LENGTH; i >= 0; --i) {
+            a = char_to_num(result.get_one_char(i));
+            b = char_to_num(argument_copy.get_one_char(i));
+            a = a + b + over;
+            result.set_one_char(i, a % 10);
+            over = a / 10;
+        }
+        //     std::cout << result.get_one_char(0) << "result0" << std::endl;
+        //      std::cout << argument_copy.get_one_char(0) << "a_to_copy0" << std::endl;
+        if (result.get_one_char(0) != '0') {
+            result.set_one_char(0, 9);
+            for (int i = 1; i <= MAX_LENGTH; ++i) {
+                result.set_one_char(i, 9 - char_to_num(result.get_one_char(i)));
+            }
+            ++result;
+
+        }
+        return result;
+    }
+
+    const Biglong Biglong::operator-(const Biglong &argument) const {
+        Biglong argument_copy = argument;
+        if (argument.get_one_char(0) == '0')
+            argument_copy.set_one_char(0, 9);
+        else
+            argument_copy.set_one_char(0, 0);
+        return (*this + argument_copy);
+    }
+
+
+    void Biglong::set_one_char(int number, int value) {
+        Biglong::value[number] = num_to_char(value);
+    }
+
+    const Biglong Biglong::operator=(const Biglong &argument) {
+        for (int i = 0; i <= MAX_LENGTH; ++i) {
+            (*this).value[i] = argument.value[i];
+        }
+        (*this).length = argument.length;
+    }
+
+
+    char num_to_char(int num) {
+        switch (num) {
+            case 0:
+                return '0';
+            case 1:
+                return '1';
+            case 2:
+                return '2';
+            case 3:
+                return '3';
+            case 4:
+                return '4';
+            case 5:
+                return '5';
+            case 6:
+                return '6';
+            case 7:
+                return '7';
+            case 8:
+                return '8';
+            case 9:
+                return '9';
+        }
+        return '-';
+    }
+
+    int char_to_num(char a) {
+        switch (a) {
+            case '0':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+        }
+        return
+                '-';
+    }
+
+
+    char *readln(void) {
+        fseek(stdin, 0, SEEK_END);
+
+        //   printf("введите информацию\n");
+        char *ptr = (char *) malloc(1);
+        *ptr = '\0';
+        char buf[101];
+        int n, len = 0;
+        do {
+            n = scanf("%100[^\n]", buf);
+            //printf("buff = %s\n",buf);
+            if (n < 0) {
+                free(ptr);
+                ptr = NULL;
+                continue;
+            }
+            if (n == 0)
+                scanf("%*c");
+            else {
+                len += strlen(buf);
+                // printf("len = %d\n",len);
+                ptr = (char *) realloc(ptr, len + 1);
+                strcat(ptr, buf);
+            }
+        } while (n > 0);
+        return ptr;
+    }
+}
---
+++ lab3byte2/prog3main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include "biglong.h"
+
+const char* messages1_0[] = {"Что вы хотите сделать?",
+                             "0) Завершить тестирование",
+                             "1) Изменить/посмотреть имеющийся объект",
+                             "2) Инициализировать второй объект и проводить операции с первым"};
+const char* messages1_1[] = {"0) Завершить тестирование",
+                             "1) Изменить один элемент объекта",
+                             "2) Инициализировать новый объект новым значением long и присвоить его этому",
+                             "3) Инициализировать новый объект строкой char* и присвоить его этому",
+                             "4) Инициализировать новый объект старым объектом и присвоить его старому",
+                             "5) Узнать состояние объекта"};
+const char* messages1_2[] = {"0) Завершить тестирование",
+                             "1) Прибавить этот объект первому",
+                             "2) Отнять первый объект от этого",
+                             "3) Отнять от первого объекта этот объект"};
+
+const int c_messages1_0 = sizeof(messages1_0) / sizeof(messages1_0[0]);
+const int c_messages1_1 = sizeof(messages1_1) / sizeof(messages1_0[0]);
+const int c_messages1_2 = sizeof(messages1_2) / sizeof(messages1_0[0]);
+
+int main() {
+    int choice1_0 = -1;
+    int choice1_1 = 0;
+    int choice1_2 = 0;
+    std::cout << "Добро пожаловать в диалоговую тестировочную программу класса BigLong" << std::endl;
+    laba3bit1::Biglong main_tester;               //объект по умолчанию
+    while (1) {
+        choice1_0 = -1;
+        choice1_1 = 0;
+        choice1_2 = 0;
+        std::cout << "Инициализорован 1 объект по умолчанию, вы можете корректировать его и/"
+                     "или добавить еще 1 объект и проводить с ними операции" << std::endl;
+        while (choice1_0 == -1) {
+            for (int i = 0; i < c_messages1_0; ++i) {
+                std::cout << messages1_0[i] << std::endl;
+            }
+            laba3bit1::getNum(choice1_0);
+            if (choice1_0 == 0) return 0;
+            if ((choice1_0 > c_messages1_0) || (choice1_0 < 1)) {
+                choice1_0 = -1;
+            }
+        }
+        if (choice1_0 == 0) return 0;
+        if (choice1_0 == 1) {
+            while (choice1_1 == 0) {
+                for (int i = 0; i < c_messages1_1; ++i) {
+                    std::cout << messages1_1[i] << std::endl;
+                }
+                laba3bit1::getNum(choice1_1);
+                if ((choice1_0 > c_messages1_1) || (choice1_0 < 1)) {
+                    choice1_1 = 0;
+                    std::cout << "Попробуйте ввести еще раз" << std::endl;
+                }
+            }
+            if (choice1_1 == 0) return 0;
+            if (choice1_1 == 1) {
+                int number = 0, value = 0;
+                std::cout << "Введите позицию изменяемого элемента и значение, на которое вы хотите его поменять"
+                          << std::endl;
+                laba3bit1::getNum(number);
+                laba3bit1::getNum(value);
+                main_tester.set_one_char(number, value);
+                continue;
+            }
+            if (choice1_1 == 2) {
+                std::cout << "Введите число" << std::endl;
+                long init = 0;
+                laba3bit1::getNum(init);
+                laba3bit1::Biglong newest(init);
+                main_tester = newest;
+                continue;
+            }
+            if (choice1_1 == 3) {
+                char* stroka;
+                std::cout << "Введите строку" << std::endl;
+                stroka = laba3bit1::readln();
+                if (strlen(stroka) > main_tester.get_max_value()) {
+                    std::cout << "Вы ввели слишком длинную строку, возвращаюсь в начало меню" << std::endl;
+                    continue;
+                }
+                laba3bit1::Biglong newest(stroka);
+                main_tester = newest;
+                continue;
+            }
+            if (choice1_1 == 4) {
+                laba3bit1::Biglong newest(main_tester);
+                main_tester = newest;
+                continue;
+            }
+            if (choice1_1 == 5) {
+                std::cout <<  "Вот текущее состояние рабочего объекта:" << main_tester << std::endl;
+                continue;
+            }
+            continue;
+        }
+        if (choice1_0 == 2) {
+            while (choice1_2 == 0) {
+                for (int i = 0; i < c_messages1_2; ++i) {
+                    std::cout << messages1_2[i] << std::endl;
+                }
+                laba3bit1::getNum(choice1_2);
+                if ((choice1_0 > c_messages1_2) || (choice1_2 < 1)) {
+                    choice1_2 = 0;
+                    std::cout << "Попробуйте ввести еще раз" << std::endl;
+                }
+            }
+            if (choice1_2 == 0) return 0;
+            std::cout << "Введите строку, с помощью которой будет инициализирован второй объект" << std::endl;
+            char* stroka;
+            stroka = laba3bit1::readln();
+            if (strlen(stroka) > main_tester.get_max_value()) {
+                std::cout << "Вы ввели слишком длинную строку, возвращаюсь в начало меню" << std::endl;
+                continue;
+            }
+            laba3bit1::Biglong newest(stroka);
+            if (choice1_2 == 1) {
+                newest = main_tester + newest;
+            }
+            if (choice1_2 == 2) {
+                newest = newest - main_tester;
+            }
+            if (choice1_2 == 3) {
+                newest = main_tester - newest;
+            }
+            std::cout << "Результат данных вычислений = " << newest << std::endl;
+        }
+    }
+
+}
